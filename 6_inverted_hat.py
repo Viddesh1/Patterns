68c3f83f151c52fbb844afdcf4035c07b7265c9d
Viddesh1 <viddeshkagithub@gmail.com>
2024-08-29 22:11:49 +0530

    Added all the files that i have worked upon using this youtube video https://youtu.be/fX64q6sYom0?si=nWud1F0v4BMFHG_8 of Simply coding

---
+++ 6_inverted_hat.py
@@ -0,0 +1,19 @@
+#  *********
+#   *******
+#    *****
+#     ***
+#      *
+
+n = 5
+
+for i in range(5):
+    for j in range(i+1):
+        print(" ", end="")
+
+    for k in range(i, n):
+        print("*", end="")
+
+    for m in range(i, n-1):
+        print("*", end="")
+
+    print()
