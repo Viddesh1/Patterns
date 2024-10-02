561bafc466852ed6dd3967cc689466997752aa9c
Viddesh1 <viddeshkagithub@gmail.com>
2024-10-02 13:32:28 +0530

    Updated the first program with added comments for debug

--- 1_simple_star_pyramid.py
+++ 1_simple_star_pyramid.py
@@ -4,8 +4,15 @@
 # ****
 # *****
 
+# breakpoint()
 for i in range(5):
     for j in range(i+1):
         print('*', end="")
 
     print()
+
+# ---------------------------------------
+# How to debug?
+# Create a breakpoint where you want to be.
+# python -m pdb my_script_name.py
+# ---------------------------------------
