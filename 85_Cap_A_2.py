b4cd13b8767234aadcc66b5a94980f34b2a35c04
Viddesh1 <viddeshkagithub@gmail.com>
2025-04-19 08:57:01 -0700

    Added the capital A and capital A (Square like)
    https://youtu.be/rUy_Nq9DHTs?si=_zU-cBH3601BHslN

---
+++ 85_Cap_A_2.py
@@ -0,0 +1,68 @@
+r"Pattern of capital A (Like a square)"
+
+# for row in range(7):
+#     for col in range(5):
+#         if  ((col==0 or col==4) and row!=0) or ((row==0 or row==3) and  (col>0 and col<4)):
+#             print("*", end="")
+#         else:
+#             print(end=" ")
+#     print()
+
+
+# for row in range(7):
+#     for col in range(5):
+#         print((row, col, "*"), end=" ")
+#     print()
+
+
+# print("##############")
+# for row in range(7):
+#     for col in range(5):
+#         if  ((col==0 or col==4) and row!=0):
+#             print((row, col, "*"), end=" ")
+#     print()
+
+
+# print("##############")
+# for row in range(7):
+#     for col in range(5):
+#         if  (row==0 or row==3):
+#             print((row, col), end=" ")
+#     print()
+
+
+
+ 
+# print("##############")
+# for row in range(7):
+#     for col in range(5):
+#         if  ((row==0 or row==3) and  (col==0 and col==4)):
+#             print((row, col), end=" ")
+#     print()
+
+
+
+# for row in range(7):
+#     for col in range(5):
+#         if  ((col==0 or col==4) and row!=0) or ((row==0 or row==3) and  (col>0 and col<4)):
+#             print((row, col, "*"), end="")
+#         else:
+#             print(end=" ")
+#     print()
+
+
+for row in range(7):
+    for col in range(5):
+        if  ((col==0 or col==4)) or ((row==0 or row==3) and  (col>0 and col<4)):
+            print("*", end="")
+        else:
+            print(end=" ")
+    print()
+
+# *****
+# *   *
+# *   *
+# *****
+# *   *
+# *   *
+# *   *
