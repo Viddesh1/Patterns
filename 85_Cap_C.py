b9dea07589f4fd57e00cc583726efca5d490a81f
Viddesh <69793689+Viddesh1@users.noreply.github.com>
2025-04-29 06:01:36 +0000

    Added the Captial B Square and Round.

---
+++ 85_Cap_C.py
@@ -0,0 +1,19 @@
+r"Pattern of capital C (Like a square)"
+
+
+for row in range(7):
+    for col in range(5):
+        if (row == 0) or (col == 0) or (row == 6):
+            print("*", end="")
+        else:
+            print(end="")
+    print()
+
+
+# *****
+# *
+# *
+# *
+# *
+# *
+# *****
