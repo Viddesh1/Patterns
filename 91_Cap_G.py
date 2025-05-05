50752ca8a90ed1e5aa3c90eb56cd36f98da12e73
Viddesh <69793689+Viddesh1@users.noreply.github.com>
2025-05-05 06:08:00 +0000

    Added square and round capital G

---
+++ 91_Cap_G.py
@@ -0,0 +1,49 @@
+r"""Pattern for capital G"""
+
+# G (Square)
+for i in range(0, 10):
+    for j in range(0, 10):
+        print((i, j, "*"), end="")
+    print(end="\n")
+
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (i == 0) or (j == 0) or (i == 9) or (j == 9 and i >=4) or (i == 4 and j >= 4):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print(end="\n")
+
+# **********
+# *         
+# *         
+# *         
+# *   ******
+# *        *
+# *        *
+# *        *
+# *        *
+# **********
+
+print("#"*50)
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (i == 0 and j >= 1) or (j == 0 and i >= 1 and i <= 8) or (i == 9 and j >= 1 and j <=8) or (j == 9 and i >=5 and i <= 8) or (i == 4 and j >= 4 and j <=8):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print(end="\n")
+
+
+#  *********
+# *         
+# *         
+# *         
+# *   ***** 
+# *        *
+# *        *
+# *        *
+# *        *
+#  ******** 
