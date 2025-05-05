aaa122a7687339abf43efcd868901c2763ba208e
Viddesh <69793689+Viddesh1@users.noreply.github.com>
2025-05-05 05:43:29 +0000

    Added the Capital E

---
+++ 89_Cap_E.py
@@ -0,0 +1,29 @@
+r"""Pattern for capital D"""
+
+# For reasoning
+# for i in range(10):
+#     for j in range(10):
+#         print((i, j, "*"), end="")
+#     print(end="\n")
+
+
+# Implementation
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (i == 0) or (i == 4) or (j == 0) or (i == 9):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print(end="\n")
+
+
+# **********
+# *         
+# *         
+# *         
+# *         
+# **********
+# *         
+# *         
+# *         
+# **********
