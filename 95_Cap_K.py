f851a8409daec1cc3d837af063c2324cdaf8cd00
Viddesh <69793689+Viddesh1@users.noreply.github.com>
2025-05-09 04:58:39 +0000

    Added Capital K
    https://www.youtube.com/watch?v=CEu21niRvYM

--- 95_Cap_K.py
+++ 95_Cap_K.py
@@ -8,10 +8,33 @@ for i in range(0, 10):
 
 
 # TODO: Capital K
-for i in range(0, 10):
-    for j in range(0, 5):
-        if (j == 0) or (i + j == 4) or (i + j == ):
+# for i in range(0, 10):
+#     for j in range(0, 5):
+#         if (j == 0) or (i + j == 4) : # or (i + j == )
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print(end="\n")
+
+i = 0
+j = 4
+for row in range(7):
+    for col in range(5):
+        if col == 0 or (row == col + 2 and col > 1):
             print("*", end="")
+        elif ((row == i and col == j) and col > 0):
+            print("*", end="")
+            i = i + 1
+            j = j - 1
         else:
-            print(" ", end="")
-    print(end="\n")
+            print(end=" ")
+    print()
+
+
+# *   *
+# *  * 
+# * *  
+# **   
+# * *  
+# *  * 
+# *   *
